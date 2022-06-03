5f646315a58aa8d72e8c59203b625d6dbb2cafe5
Phạm Minh Nghĩa <nghiaphamln3@gmail.com>
2022-06-04 00:08:25 +0700

    make it better

--- sql_app/database.py
+++ sql_app/database.py
@@ -13,6 +13,7 @@ host = os.getenv('host')
 database = os.getenv('database')
 
 SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{username}:{password}@{host}/{database}?charset=utf8'
+print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

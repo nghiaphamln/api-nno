874f5712536fbf5431ecd4e7da9b50b851a226f3
Phạm Minh Nghĩa <nghiaphamln3@gmail.com>
2022-06-03 17:40:15 +0700

    cập nhật api đăng ký tài khoản
    chỉnh sửa tên table và model

--- api/crud/account_crud.py
+++ api/crud/account_crud.py
@@ -5,10 +5,10 @@ import bcrypt
 
 
 def create_account(database: Session, account: account_schema.UserSchema):
-    if database.query(models.Users).filter_by(username=account.username).first() is not None:
+    if database.query(models.User).filter_by(username=account.username).first() is not None:
         return False
     database.add(
-        models.Users(
+        models.User(
             username=account.username,
             password=bcrypt.hashpw(str.encode(account.password), bcrypt.gensalt(14)),
             fullname=account.fullname
--- api/sql_app/models.py
+++ api/sql_app/models.py
@@ -2,8 +2,8 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sql_app.database import Base
 
 
-class Users(Base):
-    __tablename__ = 'users'
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(32), unique=True, index=True)
     password = Column(String(64))
